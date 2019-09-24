51ea8e5efda17d34162bd136effdca54b418de4c
Jason Huang <cjhuang@oath.com>
2019-09-24 23:30:11 +0800

    Updates

---
+++ precache-manifest.1cc03fa5b04a3466fd809738fb91051a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6a3405b002ffbeb7512a8f26b3cabab",
+    "url": "/badge225/index.html"
+  },
+  {
+    "revision": "7d7c5fe4c57c20d03cf1",
+    "url": "/badge225/static/css/main.c43334a7.chunk.css"
+  },
+  {
+    "revision": "9886e93d141843d06d03",
+    "url": "/badge225/static/js/2.521d2c41.chunk.js"
+  },
+  {
+    "revision": "7d7c5fe4c57c20d03cf1",
+    "url": "/badge225/static/js/main.831b9eb9.chunk.js"
+  },
+  {
+    "revision": "40f224d8d284bd95068a",
+    "url": "/badge225/static/js/runtime-main.c8ad37f7.js"
+  },
+  {
+    "revision": "8505cf606107ec8ece41d5db30d11616",
+    "url": "/badge225/static/media/Yahoo-Sans-Bold.8505cf60.woff"
+  },
+  {
+    "revision": "ecac9a0ea067e2948884a9404afad640",
+    "url": "/badge225/static/media/logo.ecac9a0e.svg"
+  },
+  {
+    "revision": "8690fb364ea2c503ddafe183a9102a0e",
+    "url": "/badge225/static/media/sana.8690fb36.png"
+  }
+]);
